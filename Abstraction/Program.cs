290344e3ce640a892de9019563a3d235fd57c696
Administrator <Administrator@CHAUHAN>
2022-03-10 22:43:48 +0530

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Shape               // abstract base class
+    {
+        public abstract void draw();             // abstract method with no body therefore use ;
+    }
+
+    public class Rectangle : Shape          // derived class : base class
+    {
+        public override void draw()                  // same method name with override keyword
+        {
+            Console.WriteLine(" Drawing Rectangle");
+            Console.ReadLine();
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Circle");
+            Console.ReadLine();
+        }
+    }
+    public class TestAbstract
+    {
+        public static void Main(string[] args)
+        {
+            Shape s;
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+        }
+    }
+}
---
+++ Abstraction/interface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    interface IShape       // interface - a class with pubic & abstract property
+    {
+        void draw();       // interface method have public & abstract
+    }
+
+    public class Rectangle : IShape       // derived class : interface class
+    {
+        public void draw()                 // derived class method
+        {
+            Console.WriteLine("Drawing Rectangle");
+            Console.ReadLine();
+        }
+    }
+
+    public class Circle : IShape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Drawing Circle");
+            Console.ReadLine();
+        }
+    }
+
+    public class TestInterface
+    {
+        public static void Main(string[] args)
+        {
+            IShape s;                   // interface object created
+            s = new Rectangle();        // object = derived class
+            s.draw();
+            s = new Circle();
+            s.draw();
+        }
+    }
+}
